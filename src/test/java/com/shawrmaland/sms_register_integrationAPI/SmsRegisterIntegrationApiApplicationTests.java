63468c7bf681463c199e4f2dae871d79742b078f
Ahmed Al Salih <a.alsalih2@gmail.com>
2025-01-15 21:18:47 -0500

    init project for the restful API

---
+++ src/test/java/com/shawrmaland/sms_register_integrationAPI/SmsRegisterIntegrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shawrmaland.sms_register_integrationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsRegisterIntegrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
